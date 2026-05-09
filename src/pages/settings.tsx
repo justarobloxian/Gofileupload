c2f6c043470d87c050e4d1288cc35d672cfa6e40
justarobloxian <jerzydelosreyes@gmail.com>
2026-05-09 23:39:40 +0800

    Refactor storage properties and update FormSwitch handlers

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -4,25 +4,25 @@ const { FormSection, FormSwitch } = Forms;
 
 export default () => {
   storage.alwaysUpload ??= false;
-  storage.copyToClipboard ??= true;
-  storage.insertIntoMessage ??= false;
+  storage.copy ??= true;
+  storage.insert ??= false;
 
   return (
     <FormSection title="Gofile Settings">
       <FormSwitch
         label="Always upload to hoster"
         value={storage.alwaysUpload}
-        onValueChange={(v) => storage.alwaysUpload = v}
+        onValueChange={(v: boolean) => storage.alwaysUpload = v}
       />
       <FormSwitch
         label="Copy link to clipboard"
-        value={storage.copyToClipboard}
-        onValueChange={(v) => storage.copyToClipboard = v}
+        value={storage.copy}
+        onValueChange={(v: boolean) => storage.copy = v}
       />
       <FormSwitch
         label="Insert into message"
-        value={storage.insertIntoMessage}
-        onValueChange={(v) => storage.insertIntoMessage = v}
+        value={storage.insert}
+        onValueChange={(v: boolean) => storage.insert = v}
       />
     </FormSection>
   );
