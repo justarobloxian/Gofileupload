6a6632ea12f03d9564647640b859e5344557a419
justarobloxian <jerzydelosreyes@gmail.com>
2026-05-09 22:45:56 +0800

    Add Rollup configuration for building project

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import esbuild from 'rollup-plugin-esbuild';
+
+export default {
+  input: 'src/index.tsx',
+  output: {
+    file: 'dist/index.js',
+    format: 'iife',
+    compact: true
+  },
+  plugins: [
+    esbuild({
+      minify: true,
+      target: 'es2020',
+      jsx: 'transform',
+    })
+  ]
+};
