7a408ecd06b1f9c043055d18546e7c395fff878e
justarobloxian <jerzydelosreyes@gmail.com>
2026-05-10 13:15:13 +0800

    Simplify rollup.config.js output and plugins

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,6 @@ import esbuild from 'rollup-plugin-esbuild';
 
 export default {
   input: 'src/index.tsx',
-  output: {
-    file: 'dist/index.js',
-    format: 'iife',
-    compact: true
-  },
-  plugins: [
-    esbuild({
-      minify: true,
-      target: 'es2020',
-      jsx: 'transform'
-    })
-  ]
+  output: { file: 'dist/index.js', format: 'iife', compact: true },
+  plugins: [esbuild({ minify: true, target: 'es2020', jsx: 'transform' })]
 };
